40f773c2fd9985b308b7f513ab3e045c07346daf
liulujie <LuJieWanAn@163.com>
2020-03-02 10:45:42 +0800

    Initial commit

---
+++ excercise_01.py
@@ -0,0 +1,14 @@
+# 打印自己的英文昵称
+# 打印一个含有加减乘除的数学表达式
+# 使用两次print语句，但只显示一行
+# 使用一次print语句，但显示在两行中
+
+print('I am kk')
+
+# sums = (3 + 2 - 1) * 2 / 2
+print((3 + 2 - 1) * 2 / 2)
+
+print('how are you',end='')
+print('Fine')
+
+print('you are \n beautiful')
